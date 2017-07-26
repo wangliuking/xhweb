adf1ff507794f843c39944ee58227ca365a2b957
杜岩桐 <q3546@Durant>
2017-07-26 22:12:54 +0800

    初步完成Tetra告警页面（具体细节需要商议后改动），主要改动如下： 	1.修改BsAlarmMapper.xml,BsAlarmMapper.java,BsAlarmService.java, 	  BsAlarmController.java文件，主要增加具体的数据库查询等操作的相关语句以及函数。 	2.增加了页面：bsAlarm.js,bsAlarm.html,为Tetra告警页面（包含统计图表） 	3.修改了菜单页面monitor-menu.html

--- src/xh/mybatis/mapper/BsAlarmMapper.java
+++ src/xh/mybatis/mapper/BsAlarmMapper.java
@@ -1,6 +1,8 @@
 package xh.mybatis.mapper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import xh.mybatis.bean.BsAlarmBean;
 
@@ -9,5 +11,26 @@ public interface BsAlarmMapper {
 	 * 查询所有告警信息
 	 */
 	public List<BsAlarmBean> selectAllBsAlarm() throws Exception;
-
+	
+	/**
+	 * 条件查询告警信息
+	 * @param map
+	 * @return
+	 * @throws Exception
+	 */
+	public List<BsAlarmBean> selectBsAlarmList(Map<String,Object> map) throws Exception;
+	
+	/**
+	 * 警告等级统计查询
+	 * @return
+	 * @throws Exception
+	 */
+	public List<HashMap> bsAlarmLevelChart() throws Exception;
+	
+	/**
+	 * 警告类型统计查询
+	 * @return
+	 * @throws Exception
+	 */
+	public List<HashMap> bsAlarmTypeChart() throws Exception;
 }
--- src/xh/springmvc/handlers/BsAlarmController.java
+++ src/xh/springmvc/handlers/BsAlarmController.java
@@ -1,41 +1,170 @@
 package xh.springmvc.handlers;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import xh.func.plugin.FlexJSON;
+import xh.func.plugin.FunUtil;
 import xh.mybatis.bean.BsAlarmBean;
 import xh.mybatis.service.BsAlarmService;
 
 
 @Controller
+@RequestMapping(value="/bsAlarm")
 public class BsAlarmController {
+	private boolean success;
+	private FunUtil funUtil=new FunUtil();
+	protected final Log log = LogFactory.getLog(WebLogController.class);
+	private FlexJSON json=new FlexJSON();
 	
-	@RequestMapping(value="/index/ajax",method=RequestMethod.GET)
+	/**
+	 * 查询Tetra告警
+	 * @param request
+	 * 	查询条件		
+	 * 		bsId：基站ID(支持模糊查询)
+	 * 		name：基站名称(支持模糊查询)
+	 * 		dealEn：警告消息状态：
+	 * 			0：未处理
+	 * 			1：已恢复
+	 * 			3：不限制
+	 * @param response
+	 */
+	@RequestMapping(value="/list",method=RequestMethod.GET)
 	@ResponseBody
 	public void selectAllBsAlarm(HttpServletRequest request, HttpServletResponse response){
-		HashMap<String, List<BsAlarmBean>> map = new HashMap<String, List<BsAlarmBean>>();
+		HashMap result = new HashMap();
 		BsAlarmService bsAlarmService = new BsAlarmService();
 		try {
-			List<BsAlarmBean> list = bsAlarmService.selectAllBsAlarm();
-			map.put("data", list);
-			String data = FlexJSON.Encode(map);
-			response.setContentType("text/html;charset=UTF-8");
+			String bsId=request.getParameter("bsId");
+			String name=request.getParameter("name");
+			String dealEn=request.getParameter("dealEn");
+			int start=funUtil.StringToInt(request.getParameter("start"));
+			int limit=funUtil.StringToInt(request.getParameter("limit"));
+			Map<String, Object> map=new HashMap<String, Object>();
+			map.put("bsId", bsId);
+			map.put("name", name);
+			map.put("dealEn", dealEn);
+			map.put("start", start);
+			map.put("limit", limit);
+			
+			List<BsAlarmBean> list = bsAlarmService.selectBsAlarmList(map);
+			result.put("data", list);
+			result.put("totals", list.size());
+			
+			/**
+			 * 测试输出调试代码
+			 */
+			System.out.println("--输入的状态值-->" + dealEn);
+			System.out.println("--输入的条件值-->" + name);
+			System.out.println("--查询到的数据-->" + list.size());
+			/**
+			 * -------end----------
+			 */
+			String jsonstr = FlexJSON.Encode(result);
+			response.setContentType("application/json;charset=utf-8");
 			PrintWriter out = response.getWriter();
-			out.write(data);
+			out.write(jsonstr);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * 故障等级统计
+	 * 
+	 * @param request
+	 * @param response
+	 */
+	@RequestMapping(value = "/data/bsAlarmLevelChart", method = RequestMethod.GET)
+	public void bsAlarmLevelByChart(HttpServletRequest request,
+			HttpServletResponse response) {
+		this.success = true;
+
+		List<HashMap> list = new ArrayList<HashMap>();
+		list = BsAlarmService.bsAlarmLevelChart();
+		for (int i = 0; i < list.size(); i++) {
+			String status = list.get(i).get("name").toString();
+			HashMap mapResult = new HashMap();
+			if (status.equals("1")) {
+				mapResult.put("name", "一级故障");
+				mapResult.put("value", list.get(i).get("value").toString());
+				list.set(i, mapResult);
+			}
+			if (status.equals("2")) {
+				mapResult.put("name", "二级故障");
+				mapResult.put("value", list.get(i).get("value").toString());
+				list.set(i, mapResult);
+			}
+			if (status.equals("3")) {
+				mapResult.put("name", "三级故障");
+				mapResult.put("value", list.get(i).get("value").toString());
+				list.set(i, mapResult);
+			}
+		}
+		HashMap result = new HashMap();
+		result.put("success", success);
+		result.put("totals", "");
+		result.put("items", list);
+		response.setContentType("application/json;charset=utf-8");
+		String jsonstr = json.Encode(result);
+		log.debug(jsonstr);
+		try {
+			response.getWriter().write(jsonstr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * 故障类型统计
+	 * 
+	 * @param request
+	 * @param response
+	 */
+	@RequestMapping(value = "/data/bsAlarmTypeChart", method = RequestMethod.GET)
+	public void bsAlarmTypeByChart(HttpServletRequest request,
+			HttpServletResponse response) {
+		this.success = true;
+
+		List<HashMap> list = new ArrayList<HashMap>();
+		list = BsAlarmService.bsAlarmTypeChart();
+		for (int i = 0; i < list.size(); i++) {
+			String status = list.get(i).get("name").toString();
+			HashMap mapResult = new HashMap();
+			mapResult.put("name", status);
+			mapResult.put("value", list.get(i).get("value").toString());
+			list.set(i, mapResult);
+		}
+		HashMap result = new HashMap();
+		result.put("success", success);
+		result.put("totals", "");
+		result.put("items", list);
+		response.setContentType("application/json;charset=utf-8");
+		String jsonstr = json.Encode(result);
+		log.debug(jsonstr);
+		try {
+			response.getWriter().write(jsonstr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
