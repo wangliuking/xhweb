30a401054fc9a419d4b90737c81e88d08d5a702d
hero0802 <397216360@qq.com>
2017-08-02 12:06:11 +0800

    Signed-off-by: hero0802 <397216360@qq.com>

--- src/xh/mybatis/service/RadioStatusService.java
+++ src/xh/mybatis/service/RadioStatusService.java
@@ -1,6 +1,9 @@
 package xh.mybatis.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -9,14 +12,41 @@ import xh.mybatis.mapper.RadioStatusMapper;
 import xh.mybatis.tools.MoreDbTools;
 
 public class RadioStatusService {
-
-	public List<RadioStatusBean> selectAllRadioStatus() throws Exception{
-		SqlSession session=MoreDbTools.getSession(MoreDbTools.DataSourceEnvironment.slave);
-		RadioStatusMapper mapper=session.getMapper(RadioStatusMapper.class);
-	        List<RadioStatusBean> RadioStatus=mapper.selectAllRadioStatus();
-	        session.commit();  
-	        session.close();
-	        return RadioStatus;   
+	/**
+	 * 查询基站下的注册终端列表
+	 * @param map
+	 * @return
+	 */
+	public static List<HashMap<String,Object>>oneBsRadio(Map<String,Object> map){
+		SqlSession sqlSession=MoreDbTools.getSession(MoreDbTools.DataSourceEnvironment.slave);
+		RadioStatusMapper mapper=sqlSession.getMapper(RadioStatusMapper.class);
+		 List<HashMap<String,Object>> list=new ArrayList<HashMap<String,Object>>();
+		 try {
+			list=mapper.oneBsRadio(map);
+			sqlSession.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 return list;
+	}
+	/**
+	 * 查询基站下的注册终端总数
+	 * @param bsId
+	 * @return
+	 */
+	public static int oneBsRadioCount(int bsId){
+		SqlSession sqlSession=MoreDbTools.getSession(MoreDbTools.DataSourceEnvironment.slave);
+		RadioStatusMapper mapper=sqlSession.getMapper(RadioStatusMapper.class);
+		int count=0;
+		 try {
+			count=mapper.oneBsRadioCount(bsId);
+			sqlSession.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 return count;
 	}
 
 }
