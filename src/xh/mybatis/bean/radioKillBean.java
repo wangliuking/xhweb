0684f2a642b5a753293e099ec50e4ea5b9a9ba96
hero0802 <mw0802@sina.cn>
2017-09-29 16:11:45 +0800

    Signed-off-by: hero0802 <mw0802@sina.cn>

---
+++ src/xh/mybatis/bean/radioKillBean.java
@@ -0,0 +1,29 @@
+package xh.mybatis.bean;
+
+public class radioKillBean {
+	
+	private int mscId=1;
+	private int userId;
+	private int killCmd;
+	public int getMscId() {
+		return mscId;
+	}
+	public void setMscId(int mscId) {
+		this.mscId = mscId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getKillCmd() {
+		return killCmd;
+	}
+	public void setKillCmd(int killCmd) {
+		this.killCmd = killCmd;
+	}
+	
+	
+
+}
