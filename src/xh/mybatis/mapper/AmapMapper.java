14915c712d1eea838d85c8dda856691a7c44b008
wangliuking <1287818687@qq.com>
2017-12-06 17:03:29 +0800

    2017/12/6 last commit by wlk

--- src/xh/mybatis/mapper/AmapMapper.java
+++ src/xh/mybatis/mapper/AmapMapper.java
@@ -6,15 +6,10 @@ import java.util.Map;
 
 public interface AmapMapper {
 	/**
-	 * 根据所选区域查询所有基站
+	 * 根据所选条件查询所有基站
 	 * @author wlk
 	 */
-	public List<HashMap<String,String>> bsByArea(List<String> zone) throws Exception;
-	/**
-	 * 根据所选级别查询所有基站
-	 * @author wlk
-	 */
-	public List<HashMap<String,String>> bsByLevel(List<String> level) throws Exception;
+	public List<HashMap<String,String>> bsByBoth(Map<String,List<String>> map) throws Exception;
 	/**
 	 * 不规则圈选基站查询
 	 * @return
