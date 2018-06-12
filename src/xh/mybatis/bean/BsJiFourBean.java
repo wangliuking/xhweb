3a97baea2c0df5eb1f3a3d5e8f1dc66980fd8d0c
hero0802 <mw0802@sina.cn>
2018-06-12 17:55:03 +0800

    交流电告警

---
+++ src/xh/mybatis/bean/BsJiFourBean.java
@@ -0,0 +1,36 @@
+package xh.mybatis.bean;
+
+public class BsJiFourBean {
+	
+	private String fsuId;
+	private String deviceId;
+	private String updateTime;
+	private int singleValue;
+	public String getFsuId() {
+		return fsuId;
+	}
+	public void setFsuId(String fsuId) {
+		this.fsuId = fsuId;
+	}
+	public String getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	public int getSingleValue() {
+		return singleValue;
+	}
+	public void setSingleValue(int singleValue) {
+		this.singleValue = singleValue;
+	}
+	
+	
+
+}
