9a4922b6a45e25e9a8acba9e94864c2db4f19dc9
hero0802 <mw0802@sina.cn>
2018-08-14 10:12:29 +0800

    Signed-off-by: hero0802 <mw0802@sina.cn>

--- src/com/activemq/TestMq.java
+++ src/com/activemq/TestMq.java
@@ -1,5 +1,16 @@
 package com.activemq;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class TestMq {
+	 //1、创建工厂连接对象，需要制定ip和端口号
+ 
 
 }
