29eb6f39b064a86351759ba48894f405085fef60
hero0802 <mw0802@sina.cn>
2019-03-14 15:42:42 +0800

    Signed-off-by: hero0802 <mw0802@sina.cn>

--- src/xh/org/listeners/WriteBsFaultListenner.java
+++ src/xh/org/listeners/WriteBsFaultListenner.java
@@ -23,117 +23,126 @@ import xh.mybatis.bean.EmhThreeBean;
 import xh.mybatis.bean.ThreeEmhAlarmBean;
 import xh.mybatis.service.BsAlarmService;
 import xh.mybatis.service.BsStatusService;
+import xh.mybatis.service.BsstationService;
 import xh.mybatis.service.DispatchStatusService;
 import xh.mybatis.service.PublicVariableService;
 import xh.mybatis.service.SqlServerService;
 
+public class WriteBsFaultListenner implements ServletContextListener {
 
-public class WriteBsFaultListenner implements ServletContextListener{
-	
-	private Timer timer=null;
+	private Timer timer = null;
 	protected final Log log4j = LogFactory.getLog(WriteBsFaultListenner.class);
-	
-
-
-	
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
-		if(timer!=null){
+		if (timer != null) {
 			timer.cancel();
 		}
-		
+
 	}
+
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
-		
-		if(timer==null){
-			timer=new Timer();
-			timer.scheduleAtFixedRate(new SfAlarm(), 10000, 4*60*60*1000+20*60*1000);
-			timer.scheduleAtFixedRate(new EmhEpsWater(), 25000, 1000*60*3);			
-			timer.scheduleAtFixedRate(new VoiceAlarm(), 15000, 1000*15);			
-			timer.scheduleAtFixedRate(new PullEmhThreeAlarm(), 20000, 1000*60*1);
-			timer.scheduleAtFixedRate(new VoiceNotCkeck(), 10000, 1*60*60*1000);
+
+		if (timer == null) {
+			timer = new Timer();
+			 timer.scheduleAtFixedRate(new SfAlarm(), 10000,
+			 4*60*60*1000+20*60*1000);
+			timer.scheduleAtFixedRate(new EmhEpsWater(), 25000, 1000*60*3);
+			timer.scheduleAtFixedRate(new VoiceAlarm(), 15000, 1000*15);
+			timer.scheduleAtFixedRate(new PullThreeEmh(), 20000, 1000 * 60 * 1);
+			timer.scheduleAtFixedRate(new VoiceNotCkeck(), 10000,
+			1*60*60*1000);
 			timer.scheduleAtFixedRate(new VoiceNotOrder(), 10000, 5*60*1000);
 		}
-		
+
 	}
-	
-	
 
 }
-class VoiceNotCkeck extends TimerTask{
-	protected final Log log=LogFactory.getLog(VoiceNotCkeck.class);
+
+class VoiceNotCkeck extends TimerTask {
+	protected final Log log = LogFactory.getLog(VoiceNotCkeck.class);
 
 	@Override
 	public void run() {
 		Constants.setBS_NOT_CHECK_NUM(BsStatusService.not_check_bs());
 	}
-	
+
 }
-class VoiceNotOrder extends TimerTask{
-	protected final Log log=LogFactory.getLog(VoiceNotOrder.class);
+
+class VoiceNotOrder extends TimerTask {
+	protected final Log log = LogFactory.getLog(VoiceNotOrder.class);
 
 	@Override
 	public void run() {
 		Constants.setBS_NOT_ORDER_NUM(BsStatusService.not_order_bs());
 	}
 }
-//四期交流电告警
-class EmhEpsWater extends TimerTask{
-	protected final Log log4j = LogFactory.getLog( EmhEpsWater.class);
+
+// 四期交流电告警
+class EmhEpsWater extends TimerTask {
+	protected final Log log4j = LogFactory.getLog(EmhEpsWater.class);
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		Long start=System.currentTimeMillis();
+		Long start = System.currentTimeMillis();
 		log4j.info("=========================================");
 		log4j.info("基站四期环控水浸，交流电实时写入开始");
 		log4j.info("=========================================");
-		
+
 		BsAlarmService.bs_ji_four();
 		System.out.println("=========================================");
-		System.out.println("EmhEpsWater：end:"+(System.currentTimeMillis()-start));
+		System.out.println("EmhEpsWater：end:"
+				+ (System.currentTimeMillis() - start));
 		System.out.println("=========================================");
-		Long start2=System.currentTimeMillis();
+		Long start2 = System.currentTimeMillis();
 		BsAlarmService.bs_water_four();
 		log4j.info("=========================================");
-		log4j.info("EmhEpsWater：end:"+(System.currentTimeMillis()-start2));
+		log4j.info("EmhEpsWater：end:" + (System.currentTimeMillis() - start2));
 		log4j.info("=========================================");
-		
+
 	}
-	
+
 }
-//四方伟业告警
-class SfAlarm extends TimerTask{
-	protected final Log log4j = LogFactory.getLog( EmhEpsWater.class);
+
+// 四方伟业告警
+class SfAlarm extends TimerTask {
+	protected final Log log4j = LogFactory.getLog(EmhEpsWater.class);
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		Long start=System.currentTimeMillis();
+		Long start = System.currentTimeMillis();
 		log4j.info("=========================================");
 		log4j.info("大数据分析告警写入开始");
 		log4j.info("=========================================");
-		
+
 		BsAlarmService.insert_sf_alarm();
 		System.out.println("=========================================");
-		System.out.println("大数据分析告警写入：end:"+(System.currentTimeMillis()-start)+"ms");
+		System.out.println("大数据分析告警写入：end:"
+				+ (System.currentTimeMillis() - start) + "ms");
 		System.out.println("=========================================");
-		
+
 	}
-	
+
 }
-//获取声音告警数目
-class VoiceAlarm extends TimerTask{
-	protected final Log log4j = LogFactory.getLog( VoiceAlarm.class);
+
+// 获取声音告警数目
+class VoiceAlarm extends TimerTask {
+	protected final Log log4j = LogFactory.getLog(VoiceAlarm.class);
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		/*log4j.info("=========================================");
-		log4j.info("获取告警数量");
-		log4j.info("=========================================");*/
-		int count=0;
+		/*
+		 * log4j.info("=========================================");
+		 * log4j.info("获取告警数量");
+		 * log4j.info("=========================================");
+		 */
+		int count = 0;
 		try {
 			count = BsStatusService.alarmVoiceCount();
 		} catch (Exception e) {
@@ -141,49 +150,123 @@ class VoiceAlarm extends TimerTask{
 			e.printStackTrace();
 		}
 		PublicVariableService.setVoiceAlarmCount(count);
-		
+
 	}
-	
+
 }
-class PullThreeEmh extends TimerTask{
-	protected final Log log=LogFactory.getLog(PullEmhThreeAlarm.class);
+
+class PullThreeEmh extends TimerTask {
+	protected final Log log = LogFactory.getLog(PullEmhThreeAlarm.class);
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		Long start = System.currentTimeMillis();
 		log.info("=========================================");
 		log.info("基站三期环控告警写入开始");
 		log.info("=========================================");
 		try {
-			List<ThreeEmhAlarmBean> list=SqlServerService.perDayAlarm();
-			
-			List<String> tablelist=SqlServerService.all_table();
-			int i=0,j=0;
+			List<ThreeEmhAlarmBean> list = SqlServerService.perDayAlarm();
+
+			List<String> tablelist = SqlServerService.all_table();
+			int i = 0, j = 0;
 			for (String string : tablelist) {
-				Map<String,Object> map=new HashMap<String, Object>();
+				Map<String, Object> map = new HashMap<String, Object>();
 				map.put("table", string);
 
-				List<EmhThreeBean> list2=SqlServerService.tb_dev_info(map);
+				List<EmhThreeBean> list2 = SqlServerService.tb_dev_info(map);
 				for (EmhThreeBean emhThreeBean : list2) {
-					ThreeEmhAlarmBean bean=new ThreeEmhAlarmBean();
-					if(emhThreeBean.getAlarmState().equals("1")){
+					ThreeEmhAlarmBean bean = new ThreeEmhAlarmBean();
 					bean.setJFNode(emhThreeBean.getBsId());
-					//bean.setDevNode(emhThreeBean.getDevNode());
-						
+					bean.setDevNode(Integer.parseInt(emhThreeBean
+							.getDevNode().trim()));
+					bean.setNodeID(Integer.parseInt(emhThreeBean
+							.getNodeID().trim()));
+					bean.setDevName(emhThreeBean.getDevName());
+					bean.setAlarmText(emhThreeBean.getAlarmEX());
+
+					Map<String, Object> bsmap = BsstationService
+							.select_bs_by_bsid(emhThreeBean.getBsId());
+					bean.setName(bsmap.get("name") == null ? "" : bsmap
+							.get("name").toString());
+					bean.setDescription(bean.getName() + ","
+							+ bean.getDevName() + ","
+							+ emhThreeBean.getNodeAttribute() + ",报警");
+					if (emhThreeBean.getAlarmState() != null
+							&& emhThreeBean.getAlarmState() != "") {
+						if (emhThreeBean.getAlarmState().equals("1")) {
+							bean.setAlarmTime(FunUtil.nowDate());
+							bean.setState("0");
+							
+							if (SqlServerService.alarmExists(bean) == 0) {
+								SqlServerService.insertEmhAlarm(bean);
+								i++;
+							}
+						} else {
+							
+							bean.setRelieveTime(FunUtil.nowDate());
+							bean.setState("1");
+							if (SqlServerService.alarmExists(bean) > 0) {
+								SqlServerService.updateEmhAlarm(bean);
+								j++;
+							}
+						}
+					}else{
 					}
 				}
-					
+
 			}
-			
+			log.info("emh:3->写入=" + i);
+			log.info("emh:3->更新=" + j);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	public synchronized static void handleList(List<String> data, int threadNum) {
+		int length = data.size();
+		int tl = length % threadNum == 0 ? length / threadNum : (length
+				/ threadNum + 1);
+
+		for (int i = 0; i < threadNum; i++) {
+			int end = (i + 1) * tl;
+			ListThread thread = new ListThread("线程[" + (i + 1) + "] ", data, i
+					* tl, end > length ? length : end);
+			thread.start();
+		}
 	}
 }
-class PullEmhThreeAlarm extends TimerTask{
-	protected final Log log=LogFactory.getLog(PullEmhThreeAlarm.class);
+
+class ListThread extends Thread {
+	private String threadName;
+	private List<String> data;
+	private int start;
+	private int end;
+
+	public ListThread(String threadName, List<String> data, int start, int end) {
+		this.threadName = threadName;
+		this.data = data;
+		this.start = start;
+		this.end = end;
+	}
+
+	public void run() {
+		// TODO 这里处理数据
+		List<String> subList = data.subList(start, end)/* .add("^&*") */;
+		for (int i = 0; i < subList.size(); i++) {
+			System.out.println(threadName + "处理了" + subList.get(i));
+		}
+
+		System.out.println(threadName + "处理了" + subList.size() + "条！");
+	}
+
+}
+
+class PullEmhThreeAlarm extends TimerTask {
+	protected final Log log = LogFactory.getLog(PullEmhThreeAlarm.class);
 
 	@Override
 	public void run() {
@@ -192,84 +275,85 @@ class PullEmhThreeAlarm extends TimerTask{
 		log.info("基站三期环控告警写入开始");
 		log.info("=========================================");
 		try {
-			List<ThreeEmhAlarmBean> list=SqlServerService.perDayAlarm();
-			System.out.println("emh:3->总数="+list.size());
-			int i=0,j=0;
+			List<ThreeEmhAlarmBean> list = SqlServerService.perDayAlarm();
+			System.out.println("emh:3->总数=" + list.size());
+			int i = 0, j = 0;
 			for (ThreeEmhAlarmBean threeEmhAlarmBean : list) {
-				if(threeEmhAlarmBean.getState().equals("报警")){
+				if (threeEmhAlarmBean.getState().equals("报警")) {
 					threeEmhAlarmBean.setState("0");
-				}else{
+				} else {
 					threeEmhAlarmBean.setState("1");
 				}
-				threeEmhAlarmBean.setAlarmTime(timeFormat(threeEmhAlarmBean.getAlarmDate(), threeEmhAlarmBean.getAlarmTime()));
-				threeEmhAlarmBean.setRelieveTime(timeFormat(threeEmhAlarmBean.getRelieveDate(), threeEmhAlarmBean.getRelieveTime()));
-				threeEmhAlarmBean.setDescription(threeEmhAlarmBean.getAlarmText());
-				String[] texts=threeEmhAlarmBean.getAlarmText().split("，");
-				
-				if(texts.length>2){
-					String [] b=texts[2].split(",");
-					if(b[0].contains("通信中断")){
-						threeEmhAlarmBean.setAlarmText(texts[1]+","+b[0]);
-					}else{
+				threeEmhAlarmBean.setAlarmTime(timeFormat(
+						threeEmhAlarmBean.getAlarmDate(),
+						threeEmhAlarmBean.getAlarmTime()));
+				threeEmhAlarmBean.setRelieveTime(timeFormat(
+						threeEmhAlarmBean.getRelieveDate(),
+						threeEmhAlarmBean.getRelieveTime()));
+				threeEmhAlarmBean.setDescription(threeEmhAlarmBean
+						.getAlarmText());
+				String[] texts = threeEmhAlarmBean.getAlarmText().split("，");
+
+				if (texts.length > 2) {
+					String[] b = texts[2].split(",");
+					if (b[0].contains("通信中断")) {
+						threeEmhAlarmBean.setAlarmText(texts[1] + "," + b[0]);
+					} else {
 						threeEmhAlarmBean.setAlarmText(b[0]);
 					}
-					
+
 					threeEmhAlarmBean.setName(texts[0]);
 				}
-				
-				//log.info(threeEmhAlarmBean);
-				if(threeEmhAlarmBean.getState().equals("0")){
-					if(SqlServerService.alarmExists(threeEmhAlarmBean)==0){
+
+				// log.info(threeEmhAlarmBean);
+				if (threeEmhAlarmBean.getState().equals("0")) {
+					if (SqlServerService.alarmExists(threeEmhAlarmBean) == 0) {
 						SqlServerService.insertEmhAlarm(threeEmhAlarmBean);
-						//log.info("write:"+threeEmhAlarmBean);
-						
+						// log.info("write:"+threeEmhAlarmBean);
+
 						i++;
-				 }
-				}else{
-					if(SqlServerService.alarmExists(threeEmhAlarmBean)>0){
+					}
+				} else {
+					if (SqlServerService.alarmExists(threeEmhAlarmBean) > 0) {
 						SqlServerService.updateEmhAlarm(threeEmhAlarmBean);
-						//log.info("update"+threeEmhAlarmBean);
+						// log.info("update"+threeEmhAlarmBean);
 						j++;
 					}
-					
+
 				}
 			}
-			log.info("emh:3->写入="+i);
-			log.info("emh:3->更新="+j);
-			
-			
+			log.info("emh:3->写入=" + i);
+			log.info("emh:3->更新=" + j);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	private String timeFormat(String alarmDate,String alarmTime){
-		
-		if(alarmDate==null || alarmTime==null){
+
+	private String timeFormat(String alarmDate, String alarmTime) {
+
+		if (alarmDate == null || alarmTime == null) {
 			return null;
 		}
 
-		String a=alarmDate.split(" ")[0];
-		String[] t=alarmTime.split(":");
-		int t1=Integer.parseInt(t[0]);
-		int t2=Integer.parseInt(t[1]);
-		String h="",m="";
-		if(t1<10){
-			h="0"+t1;
-		}else{
-			h=t[0];
+		String a = alarmDate.split(" ")[0];
+		String[] t = alarmTime.split(":");
+		int t1 = Integer.parseInt(t[0]);
+		int t2 = Integer.parseInt(t[1]);
+		String h = "", m = "";
+		if (t1 < 10) {
+			h = "0" + t1;
+		} else {
+			h = t[0];
 		}
-		if(t2<10){
-			m="0"+t2;
-		}else{
-			m=t[1];
+		if (t2 < 10) {
+			m = "0" + t2;
+		} else {
+			m = t[1];
 		}
-		return a+" "+h+":"+m+":00";
+		return a + " " + h + ":" + m + ":00";
 	}
-	
-	
-	
+
 }
-
-
--- src/xh/org/listeners/test.java
+++ src/xh/org/listeners/test.java
@@ -1,122 +1,129 @@
 package xh.org.listeners;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.TimeZone;
 import java.util.regex.Pattern;
+
+import org.apache.commons.lang.ArrayUtils;
 
 import xh.func.plugin.DocConverter;
 import xh.func.plugin.FunUtil;
 
 public class test {
 
-	
-	 public static void main(String[] args) {   
-       String a=FunUtil.nowDate();
-       String[] time=a.split(" ");
-       int t2=Integer.parseInt(time[1].split(":")[1]);
-        t2=t2%5;
-        
-        String table="xhgmnet_emh_sensor_history";
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(new Date());
-		int month = cal.get(Calendar.MONTH)+1;
-		table+=month<10?(0+String.valueOf(month)):month;
-       
-		 
-		System.out.println(false || false || false);
-		
-     }
-	 
-	 
-   public static String DataTime(){
-	   SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:00");	   
-	   
-	   String now=df.format(new Date());
-	   int m=Integer.parseInt(now.split(" ")[1].split(":")[1])%5;	   
-	   Calendar c=Calendar.getInstance();	   
-	   int x=m+20;	   
-	   //获取20分钟以前的时间
-	   c.add(Calendar.MINUTE, -x);
-	   Date d=c.getTime();
-	   return df.format(d);
-   }
-	
+	public static void main(String[] args) {
+		// 准备数据
+        List<String> data = new ArrayList<String>();
+        for (int i = 0; i < 30; i++) {
+            data.add("item" + i);
+        }
+       handleList(data, 7);
+       System.out.println(ArrayUtils.toString(data));
+	}
+
+	public synchronized static void handleList(List<String> data, int threadNum) {
+		int length = data.size();
+		int tl = length % threadNum == 0 ? length / threadNum : (length
+				/ threadNum + 1);
+
+		for (int i = 0; i < threadNum; i++) {
+			int end = (i + 1) * tl;
+			ListThread thread = new ListThread("线程[" + (i + 1) + "] ", data, i
+					* tl, end > length ? length : end);
+			thread.start();
+		}
+	}
+
+	public static String DataTime() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:00");
+
+		String now = df.format(new Date());
+		int m = Integer.parseInt(now.split(" ")[1].split(":")[1]) % 5;
+		Calendar c = Calendar.getInstance();
+		int x = m + 20;
+		// 获取20分钟以前的时间
+		c.add(Calendar.MINUTE, -x);
+		Date d = c.getTime();
+		return df.format(d);
+	}
+
 	public static String RandomWord(int num) {
 		String[] beforeShuffle = new String[] { "1", "2", "3", "4", "5", "6",
-				"7", "8", "9","0", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-				"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-				"V", "W", "X", "Y", "Z","a","b","c","d","e","f","g","h","i","j"
-				,"k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+				"7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H",
+				"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+				"U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
+				"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+				"s", "t", "u", "v", "w", "x", "y", "z" };
 		List list = Arrays.asList(beforeShuffle);
 		Collections.shuffle(list);
 		StringBuilder sb = new StringBuilder();
-		Random  random=new Random();
-		for(int i=0;i<num;i++){
-			 int s = random.nextInt(list.size()-1);
-			 sb.append(list.get(s));
+		Random random = new Random();
+		for (int i = 0; i < num; i++) {
+			int s = random.nextInt(list.size() - 1);
+			sb.append(list.get(s));
 		}
 		String afterShuffle = sb.toString();
-		
-	    
-		
-		
-		/*for (int i = 0; i < list.size(); i++) {
-			sb.append(list.get(i));
-		}
-		String afterShuffle = sb.toString();
-		String result = afterShuffle.substring(1, 9);*/
+
+		/*
+		 * for (int i = 0; i < list.size(); i++) { sb.append(list.get(i)); }
+		 * String afterShuffle = sb.toString(); String result =
+		 * afterShuffle.substring(1, 9);
+		 */
 		return afterShuffle;
 	}
-	
-	public static boolean isInteger(String str) {  
-        /*Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");*/  
+
+	public static boolean isInteger(String str) {
+		/* Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$"); */
 		Pattern pattern = Pattern.compile("[0-9]*");
-        return pattern.matcher(str).matches();  
-        
-  }
-	
-	public static String ss(){
+		return pattern.matcher(str).matches();
+
+	}
+
+	public static String ss() {
 
 		SimpleDateFormat timeF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		timeF.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));		
-		Date d=new Date();
-		String dateStr = timeF.format(d.getTime()-10*60*1000);
-		String[] time=dateStr.split(" ")[1].split(":");
-		String rtime=dateStr.split(" ")[0];
-		int a=Integer.parseInt(time[1]);
-		int c=a%5;
-		int x=a-c;
-		if(x>=10){
-			rtime+=" "+time[0]+":"+x+":00";
-		}else{
-			rtime+=" "+time[0]+":0"+x+":00";
+		timeF.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+		Date d = new Date();
+		String dateStr = timeF.format(d.getTime() - 10 * 60 * 1000);
+		String[] time = dateStr.split(" ")[1].split(":");
+		String rtime = dateStr.split(" ")[0];
+		int a = Integer.parseInt(time[1]);
+		int c = a % 5;
+		int x = a - c;
+		if (x >= 10) {
+			rtime += " " + time[0] + ":" + x + ":00";
+		} else {
+			rtime += " " + time[0] + ":0" + x + ":00";
 		}
 		return rtime;
 	}
-	
 
 }
+
 class MyTask implements Runnable {
-    private int taskNum;
- 
-    public MyTask(int num) {
-        this.taskNum = num;
-    }
- 
-    @Override
-    public void run() {
-        System.out.println("正在执行task "+taskNum);
-        try {
-            Thread.currentThread().sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("task "+taskNum+"执行完毕");
-    }
+	private int taskNum;
+
+	public MyTask(int num) {
+		this.taskNum = num;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("正在执行task " + taskNum);
+		try {
+			Thread.currentThread().sleep(4000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("task " + taskNum + "执行完毕");
+	}
 }
